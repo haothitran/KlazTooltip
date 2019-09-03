ca1af8b4e9b305c6db5098ddc23b53c2b9f7838f
Hao Tran <account@haothitran.com>
2019-09-02 20:56:27 -0400

    Convert double quotes to single quotes

--- Config.lua
+++ Config.lua
@@ -7,20 +7,20 @@ ns.C = C
 --------------------------------------------------------------------------------
 
 C.Font = {
-  ["Family"] = STANDARD_TEXT_FONT,  -- font family
-  ["Size"] = 12,                    -- font size
-  ["Style"] = "OUTLINE",            -- font outline
+  ['Family'] = STANDARD_TEXT_FONT,  -- font family
+  ['Size'] = 12,                    -- font size
+  ['Style'] = 'OUTLINE',            -- font outline
 }
 
 C.Size = {
-  ["Width"] = 125,                   -- frame width
-  ["Height"] = 25,                  -- frame height
+  ['Width'] = 125,                   -- frame width
+  ['Height'] = 25,                  -- frame height
 }
 
 C.Position = {
-	["Point"] = "BOTTOMRIGHT",             -- attachment point to parent
-	["RelativeTo"] = UIParent,      -- parent frame
-	["RelativePoint"] = "BOTTOMRIGHT",     -- parent attachment point
-	["XOffset"] = -240,                  -- horizontal offset from parent point
-	["YOffset"] = 134,               -- vertical offset from parent point
+  ['Point'] = 'BOTTOMRIGHT',             -- attachment point to parent
+  ['RelativeTo'] = UIParent,      -- parent frame
+  ['RelativePoint'] = 'BOTTOMRIGHT',     -- parent attachment point
+  ['XOffset'] = -240,                  -- horizontal offset from parent point
+  ['YOffset'] = 134,               -- vertical offset from parent point
 }
--- Tooltip.lua
+++ Tooltip.lua
@@ -5,83 +5,83 @@ local C = ns.C
 -- // TOOLTIP
 --------------------------------------------------------------------------------
 
-local loader = CreateFrame("Frame")
-loader:RegisterEvent("ADDON_LOADED")
-loader:SetScript("OnEvent", function(self, addon)
-	if addon ~= KlazTooltip then
-		local function initDB(db, defaults)
-			if type(db) ~= "table" then db = {} end
-			if type(defaults) ~= "table" then return db end
-			for k, v in pairs(defaults) do
-				if type(v) == "table" then
-					db[k] = initDB(db[k], v)
-				elseif type(v) ~= type(db[k]) then
-					db[k] = v
-				end
-			end
-		return db
-	end
+local loader = CreateFrame('Frame')
+loader:RegisterEvent('ADDON_LOADED')
+loader:SetScript('OnEvent', function(self, addon)
+  if addon ~= KlazTooltip then
+    local function initDB(db, defaults)
+      if type(db) ~= 'table' then db = {} end
+      if type(defaults) ~= 'table' then return db end
+      for k, v in pairs(defaults) do
+        if type(v) == 'table' then
+          db[k] = initDB(db[k], v)
+        elseif type(v) ~= type(db[k]) then
+          db[k] = v
+        end
+      end
+    return db
+  end
 
-		KlazTooltipDB = initDB(KlazTooltipDB, C.Position)
-		C.UserPlaced = KlazTooltipDB
-		self:UnregisterEvent("ADDON_LOADED")
-	end
+    KlazTooltipDB = initDB(KlazTooltipDB, C.Position)
+    C.UserPlaced = KlazTooltipDB
+    self:UnregisterEvent('ADDON_LOADED')
+  end
 end)
 
 --------------------------------------------------------------------------------
 -- // ANCHOR FRAME
 --------------------------------------------------------------------------------
 
-local anchor = CreateFrame("Frame", "KlazTooltipAnchor", UIParent)
+local anchor = CreateFrame('Frame', 'KlazTooltipAnchor', UIParent)
 anchor:SetSize(C.Size.Width, C.Size.Height)
 anchor:SetBackdrop({bgFile=[[Interface\MINIMAP\TooltipBackdrop-Background]]})
-anchor:SetFrameStrata("HIGH")
+anchor:SetFrameStrata('HIGH')
 anchor:SetMovable(true)
 anchor:SetClampedToScreen(true)
 anchor:EnableMouse(true)
 anchor:SetUserPlaced(true)
-anchor:RegisterForDrag("LeftButton")
-anchor:RegisterEvent("PLAYER_LOGIN")
+anchor:RegisterForDrag('LeftButton')
+anchor:RegisterEvent('PLAYER_LOGIN')
 anchor:Hide()
 
-anchor.text = anchor:CreateFontString(nil, "OVERLAY")
+anchor.text = anchor:CreateFontString(nil, 'OVERLAY')
 anchor.text:SetAllPoints(anchor)
 anchor.text:SetFont(C.Font.Family, C.Font.Size, C.Font.Style)
 anchor.text:SetShadowOffset(0, 0)
-anchor.text:SetText("KlazTooltipAnchor")
+anchor.text:SetText('KlazTooltipAnchor')
 
-anchor:SetScript("OnEvent", function(self, event, arg1)
-	if event == "PLAYER_LOGIN" then
-		self:ClearAllPoints()
-		self:SetPoint(
-		C.UserPlaced.Point,
-		C.UserPlaced.RelativeTo,
-		C.UserPlaced.RelativePoint,
-		C.UserPlaced.XOffset,
-		C.UserPlaced.YOffset)
-	end
+anchor:SetScript('OnEvent', function(self, event, arg1)
+  if event == 'PLAYER_LOGIN' then
+    self:ClearAllPoints()
+    self:SetPoint(
+    C.UserPlaced.Point,
+    C.UserPlaced.RelativeTo,
+    C.UserPlaced.RelativePoint,
+    C.UserPlaced.XOffset,
+    C.UserPlaced.YOffset)
+  end
 end)
 
-anchor:SetScript("OnDragStart", function(self)
-	self:StartMoving()
+anchor:SetScript('OnDragStart', function(self)
+  self:StartMoving()
 end)
 
-anchor:SetScript("OnDragStop", function(self)
-	self:StopMovingOrSizing()
-	self:SetUserPlaced(false)
+anchor:SetScript('OnDragStop', function(self)
+  self:StopMovingOrSizing()
+  self:SetUserPlaced(false)
 
-	point, relativeTo, relativePoint, xOffset, yOffset = self:GetPoint(1)
-		if relativeTo then
-			relativeTo = relativeTo:GetName();
-		else
-			relativeTo = self:GetParent():GetName();
-		end
+  point, relativeTo, relativePoint, xOffset, yOffset = self:GetPoint(1)
+    if relativeTo then
+      relativeTo = relativeTo:GetName();
+    else
+      relativeTo = self:GetParent():GetName();
+    end
 
-	C.UserPlaced.Point = point
-	C.UserPlaced.RelativeTo = relativeTo
-	C.UserPlaced.RelativePoint = relativePoint
-	C.UserPlaced.XOffset = xOffset
-	C.UserPlaced.YOffset = yOffset
+  C.UserPlaced.Point = point
+  C.UserPlaced.RelativeTo = relativeTo
+  C.UserPlaced.RelativePoint = relativePoint
+  C.UserPlaced.XOffset = xOffset
+  C.UserPlaced.YOffset = yOffset
 end)
 
 --------------------------------------------------------------------------------
@@ -89,9 +89,9 @@ end)
 --------------------------------------------------------------------------------
 
 hooksecurefunc('GameTooltip_SetDefaultAnchor', function(self, parent)
-	self:SetOwner(parent, "ANCHOR_NONE")
+  self:SetOwner(parent, 'ANCHOR_NONE')
   self:ClearAllPoints()
-	self:SetPoint("BOTTOMRIGHT", anchor, "BOTTOMRIGHT", 0, 0)
+  self:SetPoint('BOTTOMRIGHT', anchor, 'BOTTOMRIGHT', 0, 0)
 end)
 
 --------------------------------------------------------------------------------
@@ -99,22 +99,22 @@ end)
 --------------------------------------------------------------------------------
 
 local backdrop = {
-  bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
-	insets = {top = 1, bottom = 1, left = 1, right = 1}
+  bgFile = 'Interface\\ChatFrame\\ChatFrameBackground',
+  insets = {top = 1, bottom = 1, left = 1, right = 1}
 }
 
 local function UpdateStyle(self)
-	self:SetBackdrop(backdrop)
-	self:SetBackdropColor(0, 0, 0, .75)
+  self:SetBackdrop(backdrop)
+  self:SetBackdropColor(0, 0, 0, .75)
 end
 
 local function UpdateUnit(self)
-	local _, unit = self:GetUnit()
+  local _, unit = self:GetUnit()
   if not unit then return end
 
-	-- unit is not a player
-	if not UnitIsPlayer(unit) then
-    local reaction = UnitReaction(unit, "player")
+  -- unit is not a player
+  if not UnitIsPlayer(unit) then
+    local reaction = UnitReaction(unit, 'player')
     if reaction then
       local color = FACTION_BAR_COLORS[reaction]
       if color then
@@ -123,28 +123,28 @@ local function UpdateUnit(self)
       end
     end
 
-	-- unit is any player
-	else
-	  -- name colour
-	  local color = RAID_CLASS_COLORS[select(2, UnitClass(unit))]
-	  GameTooltipTextLeft1:SetTextColor(color.r,color.g,color.b)
-		GameTooltipStatusBar:SetStatusBarColor(color.r,color.g,color.b)
+  -- unit is any player
+  else
+    -- name colour
+    local color = RAID_CLASS_COLORS[select(2, UnitClass(unit))]
+    GameTooltipTextLeft1:SetTextColor(color.r,color.g,color.b)
+    GameTooltipStatusBar:SetStatusBarColor(color.r,color.g,color.b)
 
-	  -- guild name
-	  if GetGuildInfo(unit) then
-	    GameTooltipTextLeft2:SetTextColor(1, 0, 1)
-	  end
-	end
+    -- guild name
+    if GetGuildInfo(unit) then
+      GameTooltipTextLeft2:SetTextColor(1, 0, 1)
+    end
+  end
 
-	-- status bar
+  -- status bar
   GameTooltipStatusBar:SetHeight(2)
-  GameTooltipStatusBar:SetStatusBarTexture("Interface\\ChatFrame\\ChatFrameBackground")
+  GameTooltipStatusBar:SetStatusBarTexture('Interface\\ChatFrame\\ChatFrameBackground')
   GameTooltipStatusBar:ClearAllPoints()
-  GameTooltipStatusBar:SetPoint("LEFT", 1, 0)
-  GameTooltipStatusBar:SetPoint("RIGHT", -1, 0)
-  GameTooltipStatusBar:SetPoint("TOP", 0, -1)
+  GameTooltipStatusBar:SetPoint('LEFT', 1, 0)
+  GameTooltipStatusBar:SetPoint('RIGHT', -1, 0)
+  GameTooltipStatusBar:SetPoint('TOP', 0, -1)
 
-	self:Show()
+  self:Show()
 end
 
 --------------------------------------------------------------------------------
@@ -152,16 +152,16 @@ end
 --------------------------------------------------------------------------------
 
 for _, tooltip in next, {
-	GameTooltip,
-	WorldMapTooltip,
+  GameTooltip,
+  WorldMapTooltip,
 } do
-	tooltip:HookScript('OnShow', UpdateStyle)
-	tooltip:HookScript('OnUpdate', UpdateStyle)
-	tooltip:HookScript('OnTooltipSetUnit', UpdateUnit)
+  tooltip:HookScript('OnShow', UpdateStyle)
+  tooltip:HookScript('OnUpdate', UpdateStyle)
+  tooltip:HookScript('OnTooltipSetUnit', UpdateUnit)
 
-	for _, shoppingTooltip in next, tooltip.shoppingTooltips do
-		shoppingTooltip:HookScript('OnTooltipSetItem', UpdateStyle)
-	end
+  for _, shoppingTooltip in next, tooltip.shoppingTooltips do
+    shoppingTooltip:HookScript('OnTooltipSetItem', UpdateStyle)
+  end
 end
 
 --------------------------------------------------------------------------------
@@ -169,25 +169,25 @@ end
 --------------------------------------------------------------------------------
 
 SlashCmdList.KLAZTOOLTIP = function (msg, editbox)
-	if (string.lower(msg) == "reset") then
-		KlazTooltipDB = C.Position
-		ReloadUI()
-	elseif (string.lower(msg) == "unlock") then
-		if not anchor:IsShown() then
+  if (string.lower(msg) == 'reset') then
+    KlazTooltipDB = C.Position
+    ReloadUI()
+  elseif (string.lower(msg) == 'unlock') then
+    if not anchor:IsShown() then
       anchor:Show()
-			print("|cff1994ffKlazTooltip|r |cff00ff00Unlocked.|r")
+      print('|cff1994ffKlazTooltip|r |cff00ff00Unlocked.|r')
     end
-	elseif (string.lower(msg) == "lock") then
-		anchor:Hide()
-		print("|cff1994ffKlazTooltip|r |cffff0000Locked.|r")
-	else
-		print("------------------------------------------")
-		print("|cff1994ffKlazTooltip commands:|r")
-		print("------------------------------------------")
-		print("|cff1994ff/klaztooltip unlock|r Unlocks frame to be moved.")
-		print("|cff1994ff/klaztooltip lock|r Locks frame in position.")
-		print("|cff1994ff/klaztooltip reset|r Resets frame to default position.")
-	end
+  elseif (string.lower(msg) == 'lock') then
+    anchor:Hide()
+    print('|cff1994ffKlazTooltip|r |cffff0000Locked.|r')
+  else
+    print('------------------------------------------')
+    print('|cff1994ffKlazTooltip commands:|r')
+    print('------------------------------------------')
+    print('|cff1994ff/klaztooltip unlock|r Unlocks frame to be moved.')
+    print('|cff1994ff/klaztooltip lock|r Locks frame in position.')
+    print('|cff1994ff/klaztooltip reset|r Resets frame to default position.')
+  end
 end
 SLASH_KLAZTOOLTIP1 = '/klaztooltip'
 SLASH_KLAZTOOLTIP2 = '/ktooltip'
